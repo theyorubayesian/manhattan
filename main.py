8034c3c2104a2078ac5cb8c42ba1595121274d72
Akintunde Oladipo <akin.o.oladipo@gmail.com>
2021-06-05 17:54:40 +0100

    feat: implements data_check

--- main.py
+++ main.py
@@ -67,10 +67,17 @@ def go(config: DictConfig):
             )
 
         if "data_check" in active_steps:
-            ##################
-            # Implement here #
-            ##################
-            pass
+            _ = mlflow.run(
+                os.path.join(root_path, 'src', 'data_check'),
+                "main",
+                parameters={
+                    "csv": f"{config['etl']['output_name']}:latest",
+                    "ref": f"{config['etl']['output_name']}:reference",
+                    "kl_threshold": config["data_check"]['kl_threshold'],
+                    "min_price": config["etl"]["min_price"],
+                    "max_price": config["etl"]["max_price"]
+                }
+            )
 
         if "data_split" in active_steps:
             ##################
--- src/data_check/test_data.py
+++ src/data_check/test_data.py
@@ -68,3 +68,9 @@ def test_similar_neigh_distrib(data: pd.DataFrame, ref_data: pd.DataFrame, kl_th
 ########################################################
 # Implement here test_row_count and test_price_range   #
 ########################################################
+def test_row_count(data: pd.DataFrame):
+    assert 15000 < data.shape[0] < 1000000
+
+
+def test_price_range(data: pd.DataFrame, min_price: int, max_price: int):
+    assert all(data['price'].between(min_price, max_price))
